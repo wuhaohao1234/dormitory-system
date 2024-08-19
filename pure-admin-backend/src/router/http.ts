6a7ecb8ea3d0f662d3a11bec191e589edb755fc1
wuhaohao1234 <161149758@qq.com>
2024-08-19 08:43:14 +0800

    fix: 修复登录逻辑

--- pure-admin-backend/src/router/http.ts
+++ pure-admin-backend/src/router/http.ts
@@ -111,6 +111,7 @@ const login = async (req: Request, res: Response) => {
               // 这里模拟角色，根据自己需求修改
               roles: ["common"],
               accessToken,
+              id: data[0].id,
               // 这里模拟刷新token，根据自己需求修改
               refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
               expires: new Date(new Date()).getTime() + expiresIn,
